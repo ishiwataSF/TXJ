f271a64ecd0df0c2ea7dd802797403a84ca2f7b2
ishiwata <ishiwata@sfinter.com>
2020-11-02 16:56:07 +0900

    edited wsgi.py add.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+import pymysql
+pymysql.install_as_MySQLdb()
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
