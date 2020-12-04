6975913b0671ea69bed6e931d96d7ac07ca106ac
ishiwata <ishiwata@sfinter.com>
2020-12-04 18:11:52 +0900

    check_procedure.html delete.

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib.staticfiles.urls import static
 from django.urls import path
 from .views import HistoryListView, ImportDataCreateView, ImportDataDetailView, \
-    LoginFormView, CustomerSelectAndFileUpLoadView,MatchedDataDetailAndVisuallyMatchedDataCreateView, MatchedDataCheckProcedureTmprateView, logout
+    LoginFormView, CustomerSelectAndFileUpLoadView,MatchedDataDetailAndVisuallyMatchedDataCreateView, logout
 
 urlpatterns = [
     path('accounts/login/', LoginFormView.as_view(), name='login'),
@@ -10,7 +10,6 @@ urlpatterns = [
     path('', HistoryListView.as_view(), name='top'),
     path('file_upload/', CustomerSelectAndFileUpLoadView.as_view(), name='file_upload'),
     path('matched_data/<int:pk>/detail_and_create',MatchedDataDetailAndVisuallyMatchedDataCreateView.as_view(), name='detail_and_create'),
-    path('check_procedure/',MatchedDataCheckProcedureTmprateView.as_view(), name='check_procedure'),
     path('visually_match/<int:pk>/import_data/', ImportDataCreateView.as_view(), name='import_data'),
     path('import_data/<int:pk>/', ImportDataDetailView.as_view(), name='import_data_detail'),
 
